30b815d112df504fef7594e207d23fbab9225a1b
MarusaThem <nqthemit@gmail.com>
2018-09-06 11:50:06 +0700

    Move to script GTM to head web
    GTM move

--- themes/default/views/header.php
+++ themes/default/views/header.php
@@ -118,18 +118,19 @@ if(isset($additional_header_info))
   src="https://www.facebook.com/tr?id=199542380653225&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
-
-</head>
-<body>
 <!-- Google Tag Manager -->
-<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N7FXMB"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-N7FXMB');</script>
 <!-- End Google Tag Manager -->
+</head>
+<body>
+<!-- NoScripty Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N7FXMB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End NoScripty Google Tag Manager -->
 
 <!-- header -->
 <header class="header-wp">
