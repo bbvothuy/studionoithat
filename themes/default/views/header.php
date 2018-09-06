bba0a8a1a940b5f3a7a556e1390348b713a1de83
MarusaThem <nqthemit@gmail.com>
2018-09-06 22:36:59 +0700

    Update script  google
    Move script from footer to head of GTA (before wrrong location): http://prntscr.com/krdb6k fix done

--- themes/default/views/header.php
+++ themes/default/views/header.php
@@ -118,14 +118,32 @@ if(isset($additional_header_info))
   src="https://www.facebook.com/tr?id=199542380653225&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
+
 <!-- Google Tag Manager -->
+<!--GTM-->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-N7FXMB');</script>
+
+<!--GTA-->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-23662758-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 <!-- End Google Tag Manager -->
 </head>
+
 <body>
 <!-- NoScripty Google Tag Manager -->
 <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N7FXMB"
